ce59795ccf1c834c09122f6ed6f27441d4ff399a
Juan Pablo <juan.pablo.castano@outlook.com>
2025-06-11 17:33:44 -0500

    feat: complete 08_removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    for (let i = 0; i < theArgs.length; ++i) {
+        while (array.includes(theArgs[i])){
+            index = array.indexOf(theArgs[i]);
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line

778ef2e2b71bde61081f302e3ae2e49f8e6bcaa1
Juan Pablo <juan.pablo.castano@outlook.com>
2025-06-12 09:56:06 -0500

    feat: complete 09_sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+        if (a > 0 && b > 0) {
+            if (a < b) {
+                let sum = 0;
+                for (let i = a; i <= b; ++i) {
+                    sum += i;
+                }
+                return sum;
+            } else {
+                let sum = 0;
+                for (let i = b; i <= a; ++i) {
+                    sum += i;
+                }
+                return sum;
+            }     
+        } else {
+            return "ERROR";
+        }
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line

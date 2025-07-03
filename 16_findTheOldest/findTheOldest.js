060fed0c54b7bc08af5a03539765cb6683310432
Juan Pablo <juan.pablo.castano@outlook.com>
2025-07-03 15:40:10 -0500

    feat: complete 16_findTheOldest exercise!

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr = arr.sort((a, b) => {
+        if (typeof(a.yearOfDeath) === "undefined"){
+            const d = new Date();
+            a.yearOfDeath = d.getFullYear();
+        }
+        if (typeof(b.yearOfDeath) === "undefined"){
+            const d = new Date();
+            b.yearOfDeath = d.getFullYear();
+        }
+        aYears = a.yearOfDeath - a.yearOfBirth;
+        bYears = b.yearOfDeath - b.yearOfBirth;
+        return bYears - aYears
+    });
+    return arr[0];
 };
 
 // Do not edit below this line

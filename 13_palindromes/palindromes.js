d02309aae1ed6f8796d8f192dc15a616a5c9b0b7
Juan Pablo <juan.pablo.castano@outlook.com>
2025-07-03 11:59:41 -0500

    feat: complete 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let punctuationMarks = ["!", ".", "?",];
+    if (punctuationMarks.includes(string.at(-1))){
+        string = string.slice(0,-1);
+    }
+    string = string.toLowerCase();
+    let arr = string.split("");
+    arr = arr.filter(item => !(item === "," || item === " "));
+    string = arr.join("");
+    let arrReversed = arr.reverse();
+    let stringReversed = arrReversed.join("");
+    return string === stringReversed
 };
 
 // Do not edit below this line

2b0486cc6a532a0441c19f5b5d7379a0d9eb9750
Juan Pablo <juan.pablo.castano@outlook.com>
2025-07-03 14:55:02 -0500

    feat: complete 14_fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = +num
+    if (num === 0){
+        return 0
+    }
+    else if (num < 0) {
+        return "OOPS"
+    } else {
+        let fibonacci = [0, 1]
+        for (let i = 1; i < num ; ++i) {
+            let f1 = fibonacci[i];
+            let f2 = fibonacci[i-1];
+            let newfn = f1 + f2;
+            fibonacci.push(newfn);
+        }
+        return fibonacci.at(-1)
+    }
 
 };
 
